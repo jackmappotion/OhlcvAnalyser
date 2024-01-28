ec44c541101957b0ec089f7ca6bd6a243f03bb62
jackmappotion <jackmappotion@gmail.com>
2024-01-28 19:30:34 +0900

    :recycle: Refactor [util based -> analyser based]

--- OhlcvAnalyser/MultiOhlcvAnalyser.py
+++ OhlcvAnalyser/MultiOhlcvAnalyser.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
-from .utils import get_linear_coef,get_normalized_linear_coef
+from .analyser import CoefficientAnalyser
+from .analyser import ProfitAnalyser
 from .utils import filter_date
-from .utils import calc_profit
+
 
 class MultiOhlcvAnalyser:
     def __init__(self, multi_ohlcv) -> None:
@@ -14,33 +15,51 @@ class MultiOhlcvAnalyser:
         return groupby_ohlcv
 
     def info(self, start=None, end=None):
+        """
+        General Information
+        """
         ohlcv = filter_date(self.ohlcv, start, end)
         groupby_code = self._get_groupby_code(ohlcv, start, end)
-        ps = groupby_code['close'].apply(lambda x: calc_profit(x))
+        profit_row = groupby_code.apply(
+            lambda x: ProfitAnalyser.get_start_end_profit(x["close"])
+        )
         info_dict = {
             "total_stock_code": len(set(ohlcv["code"])),
             "start_date": ohlcv.index.min(),
             "end_date": ohlcv.index.max(),
-            "market_average_profit": (ps.mean().round(2)) * 100,
-            "increased_stock_pct": round(len(ps[ps > 0]) / len(ps), 2) * 100,
-            "decreased_stock_pct": round(len(ps[ps <= 0]) / len(ps), 2) * 100,
+            "market_average_profit": (profit_row.mean().round(2)),
+            "increased_stock_pct": round(
+                len(profit_row[profit_row > 0]) / len(profit_row), 2
+            )
+            * 100,
+            "decreased_stock_pct": round(
+                len(profit_row[profit_row <= 0]) / len(profit_row), 2
+            )
+            * 100,
         }
+
         info_df = pd.DataFrame.from_dict(
             info_dict, orient="index", columns=["value"]
         )
         return info_df
 
     def coef(self, arg, start=None, end=None):
+        """
+        Series : coefficient
+        """
         groupby_code = self._get_groupby_code(self.ohlcv, start, end)
         coef_series = groupby_code.apply(
-            lambda x: get_linear_coef(x[arg])
+            lambda x: CoefficientAnalyser.get_coefficient(x[arg])
         ).rename(f"{arg}_coef")
         return coef_series
 
     def normalized_coef(self, arg, start=None, end=None):
+        """
+        Series / Series.mean() : coefficient
+        """
         groupby_code = self._get_groupby_code(self.ohlcv, start, end)
         normalized_coef_series = groupby_code.apply(
-            lambda x: get_normalized_linear_coef(x[arg])
+            lambda x: CoefficientAnalyser.get_normalized_coefficient(x[arg])
         ).rename(f"{arg}_normalized_coef")
         return normalized_coef_series
 
@@ -67,11 +86,11 @@ class MultiOhlcvAnalyser:
             lambda x: x["hl_change"].var()
         ).rename("hl_variance")
         return hl_variance_series
-    
+
     def profit(self, start, end):
         ohlcv = self.ohlcv.copy()
         groupby_code = self._get_groupby_code(ohlcv, start, end)
         profit_series = groupby_code.apply(
-            lambda x: calc_profit(x)
+            lambda x: ProfitAnalyser.get_start_end_profit(x)
         ).rename("profit")
         return profit_series
