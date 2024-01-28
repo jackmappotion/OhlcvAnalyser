39f71b7bb1c56e99a9916fb151491de30208cc5f
jackmappotion <jackmappotion@gmail.com>
2024-01-28 09:07:39 +0900

    [INIT] SingleAnalyser

---
+++ OhlcvAnalyser/SingleOhlcvAnalyser.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from .utils import get_linear_coef,get_normalized_linear_coef
+from .utils import filter_date
+from .utils import calc_profit
+
+class SingleOhlcvAnalyser:
+    pass
